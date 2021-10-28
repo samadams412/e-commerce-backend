0d270eb62048c3bcf1822b6c320d95008e255e03
Samuel Adams <samueladams412@gmail.com>
2021-10-27 23:22:59 -0500

    initial commit

---
+++ Develop/models/Tag.js
@@ -0,0 +1,18 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Tag extends Model {}
+
+Tag.init(
+  {},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
+);
+
+module.exports = Tag;
